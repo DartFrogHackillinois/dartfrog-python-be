c2c82d6eb2bc7d06e60ec64cf45c4c29a12b211f
Edgar Babajanyan <bedgar2005@gmail.com>
2024-02-23 21:36:45 -0500

    Gemini Bridge

--- gemini_bridge.py
+++ gemini_bridge.py
@@ -2,7 +2,6 @@ import pathlib
 import textwrap
 
 import google.generativeai as genai
-from google.colab import userdata
 from IPython.display import display
 from IPython.display import Markdown
 
@@ -10,4 +9,11 @@ def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-GOOGLE_API_KEY=userdata.get('AIzaSyAV5Ee-abg_SrUP6MpoC60f3EpXysZc4N8')
+GOOGLE_API_KEY=('AIzaSyAV5Ee-abg_SrUP6MpoC60f3EpXysZc4N8')
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+response = model.generate_content("What is the meaning of life?")
+print(response.text)
