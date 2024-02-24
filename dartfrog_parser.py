6649792888c164aed3d3ef53c9e3ad3d825582f1
Pritika04 <81809982+Pritika04@users.noreply.github.com>
2024-02-23 22:29:34 -0600

    Add files via upload

--- dartfrog_parser.py
+++ dartfrog_parser.py
@@ -1,7 +1,10 @@
 import csv
 
-with open('annual-enterprise-survey-2021-financial-year-provisional.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file)
+csv_file_path = 'annual-enterprise-survey-2021-financial-year-provisional.csv'
+text_file_path = 'output_text.txt'
 
-    for row in reader:
-        print(row)
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    with open(text_file_path, 'w') as text_file:
+        for row in csv_reader:
+            text_file.write(','.join(row) + '\n')
--- gemini_bridge.py
+++ gemini_bridge.py
@@ -2,6 +2,7 @@ import pathlib
 import textwrap
 
 import google.generativeai as genai
+from google.colab import userdata
 from IPython.display import display
 from IPython.display import Markdown
 
@@ -9,17 +10,4 @@ def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-GOOGLE_API_KEY=('AIzaSyAV5Ee-abg_SrUP6MpoC60f3EpXysZc4N8')
-
-genai.configure(api_key=GOOGLE_API_KEY)
-
-model = genai.GenerativeModel('gemini-pro')
-
-
-with open('dartfrog_query.txt', 'r') as file:
-  query_text = file.read()
-
-response = model.generate_content(query_text)
-print(response.text)
-
-
+GOOGLE_API_KEY=userdata.get('AIzaSyAV5Ee-abg_SrUP6MpoC60f3EpXysZc4N8')
