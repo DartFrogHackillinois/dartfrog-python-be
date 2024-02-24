b439ef15719a89e4e53206db35fcd0f72ca80e44
Edgar Babajanyan <bedgar2005@gmail.com>
2024-02-23 21:07:14 -0500

    Colab and Gemini

--- gemini_bridge.py
+++ gemini_bridge.py
@@ -2,10 +2,12 @@ import pathlib
 import textwrap
 
 import google.generativeai as genai
-
+from google.colab import userdata
 from IPython.display import display
 from IPython.display import Markdown
 
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+GOOGLE_API_KEY=userdata.get('AIzaSyAV5Ee-abg_SrUP6MpoC60f3EpXysZc4N8')
